d15ba0a239e99a943a763dffd19a2df1eebb86d2
Alan <Alan@WIN-G9QDGPR21AM>
2017-12-11 16:07:43 -0600

    Added Text Files

--- src/Flashcard.java
+++ src/Flashcard.java
@@ -1,6 +1,10 @@
 /**
  * 
  */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import fastily.jwiki.core.Wiki;
 
 /**
  * @author Alan & Ziyang
@@ -14,7 +18,31 @@ public class Flashcard {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//test
-
+		// Gets the text of the main page and prints it.
+		//Wiki wiki = new Wiki("en.wikipedia.org");
+		//System.out.println(wiki.getPageText("Main Page"));
+		//Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
 	}
-
+	
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
 }
+
